f8b841746c317e6911d09a36d7526724bf3cd669
Veta322 <vetarejh@gmail.com>
2023-06-16 11:40:36 +0300

    add list

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,33 +1,41 @@
-
-/* Зачем здесь лишняя обертка списка? Тут обернуты в ссылки компоменты */
 function Portfolio() {
   return (
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <a
-        className="portfolio__component"
-        href="https://veta322.github.io/how-to-learn/"
-        target="blanck"
-      >
-        <p className="portfolio__text">Статичный сайт</p>
-        <p className="portfolio__arrow">↗</p>
-      </a>
-      
-      <a className="portfolio__component"
-        href="https://veta322.github.io/russian-travel/"
-        target="blanck">
-        <p className="portfolio__text">Адаптивный сайт</p>
-        <p className="portfolio__arrow">↗</p>
-      </a>
-      <a className="portfolio__component"
-        href="https://veta322.github.io/mesto/"
-        target="blanck">
-        <p className="portfolio__text">Одностраничное приложение</p>
-        <p className="portfolio__arrow">↗</p>
-      </a>
+      <ul className="portfolio__list">
+        <li className="portfolio__component">
+        <a
+          className="portfolio__link"
+          href="https://veta322.github.io/how-to-learn/"
+          target="blanck"
+        >
+          <p className="portfolio__text">Статичный сайт</p>
+          <p className="portfolio__arrow">↗</p>
+        </a>
+        </li>
+        <li className="portfolio__component">
+        <a
+          className="portfolio__link"
+          href="https://veta322.github.io/russian-travel/"
+          target="blanck"
+        >
+          <p className="portfolio__text">Адаптивный сайт</p>
+          <p className="portfolio__arrow">↗</p>
+        </a>
+        </li>
+        <li className="portfolio__component">
+        <a
+          className="portfolio__link"
+          href="https://veta322.github.io/mesto/"
+          target="blanck"
+        >
+          <p className="portfolio__text">Одностраничное приложение</p>
+          <p className="portfolio__arrow">↗</p>
+        </a>
+        </li>
+      </ul>
     </section>
   );
 }
 
 export default Portfolio;
-
